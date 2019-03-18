5ac517e6054acc41625c43ef6b7202cd12ad9b39
zlasd <zlasd@hotmail.com>
2019-03-18 20:55:03 +0800

    add baseline rule

--- detector.py
+++ detector.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 import argparse
 
 import tf_utils
+import my_utils
 
 """
 https://github.com/opencv/opencv/wiki/TensorFlow-Object-Detection-API
@@ -17,10 +18,11 @@ MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
 # MODEL_NAME = 'ssd_mobilenet_v1_0.75_depth_300x300_coco14_sync_2018_07_03'
 # MODEL_NAME = 'ssd_mobilenet_v1_ppn_shared_box_predictor_300x300_coco14_sync_2018_07_03'
 CWD_PATH = os.getcwd()
-PATH_TO_CKPT = os.path.join(CWD_PATH, 'data/model', MODEL_NAME, 'frozen_inference_graph.pb')
-IMG_NAME = random.choice(os.listdir('data/test_img'))
-IMG_PATH = os.path.join('data/test_img', IMG_NAME)
-LABELS_PATH = os.path.join(CWD_PATH,'protos','mscoco_label_map.pbtxt')
+PATH_TO_CKPT = os.path.join(CWD_PATH, 'models', MODEL_NAME, 'frozen_inference_graph.pb')
+IMG_FOLDER = os.path.join('data', 'test_img')
+IMG_NAME = random.choice(os.listdir(IMG_FOLDER))
+#IMG_PATH = os.path.join('env/img', 'zhanghua_0006.jpg')
+LABELS_PATH = os.path.join(CWD_PATH, 'data', 'mscoco_label_map.pbtxt')
 NUM_CLASSES = 90
 
 label_map = tf_utils.load_labelmap(LABELS_PATH)
@@ -31,32 +33,53 @@ category_index = tf_utils.create_category_index(categories)
 # print(categories)
 # print(category_index)
 
+def visualize_fighting(img, bboxs):
+    """ visualize fighting bounding box
+    """
+    rows, cols = img.shape[:2]
+    top = min(bboxs[0][0], bboxs[1][0]) * rows
+    left = min(bboxs[0][1], bboxs[1][1]) * cols
+    bottom = max(bboxs[0][2], bboxs[1][2]) * rows
+    right = max(bboxs[0][3], bboxs[1][3]) * cols
+    cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (0xff, 0xcc, 0x66), thickness=10)
+    
+
+
+
+
 with tf.gfile.FastGFile(PATH_TO_CKPT, 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
-    
-    
+
+
+
 with tf.Session() as sess:
     # Restore session
     sess.graph.as_default()
     tf.import_graph_def(graph_def, name='')
+    FileNames = os.listdir(os.path.join(CWD_PATH, IMG_FOLDER))
+    img_num=len(FileNames)
+    ratios=np.zeros(img_num)
 
-    # Read and preprocess an image.
-    img = cv.imread(IMG_PATH)
-    rows = img.shape[0]
-    cols = img.shape[1]
-    # inp = cv.resize(img, (300, 300))
-    inp = img[::3, ::3, :]
-    # inp = inp[:, :, [2, 1, 0]]  # BGR2RGB
+    for j in range(img_num):
+        IMG_PATH = os.path.join(IMG_FOLDER, FileNames[j])
+        # Read and preprocess an image.
+        img = cv.imread(IMG_PATH)
+        rows = img.shape[0]
+        cols = img.shape[1]
+        # inp = cv.resize(img, (300, 300))
+        inp = img[::3, ::3, :]
+        # inp = img[:,:,:]
+        # inp = inp[:, :, [2, 1, 0]]  # BGR2RGB
 
-    # Run the model
-    out = sess.run([sess.graph.get_tensor_by_name('num_detections:0'),
+        # Run the model
+        out = sess.run([sess.graph.get_tensor_by_name('num_detections:0'),
                     sess.graph.get_tensor_by_name('detection_scores:0'),
                     sess.graph.get_tensor_by_name('detection_boxes:0'),
                     sess.graph.get_tensor_by_name('detection_classes:0')],
                    feed_dict={'image_tensor:0': inp.reshape(1, inp.shape[0], inp.shape[1], 3)})
 
-    tf_utils.visualize_boxes_and_labels_on_image_array(
+        tf_utils.visualize_boxes_and_labels_on_image_array(
             inp,
             np.squeeze(out[2]),
             np.squeeze(out[3]).astype(np.int32),
@@ -65,18 +88,27 @@ with tf.Session() as sess:
             use_normalized_coordinates=True,
             line_thickness=8,
             min_score_thresh=0.40)
-    # # Visualize detected bounding boxes.
-    # num_detections = int(out[0][0])
-    # for i in range(num_detections):
-        # classId = int(out[3][0][i])
-        # score = float(out[1][0][i])
-        # bbox = [float(v) for v in out[2][0][i]]
-        # if score > 0.3:
-            # x = bbox[1] * cols
-            # y = bbox[0] * rows
-            # right = bbox[3] * cols
-            # bottom = bbox[2] * rows
-            # cv.rectangle(img, (int(x), int(y)), (int(right), int(bottom)), (125, 255, 51), thickness=2)
-
-cv.imshow('TensorFlow MobileNet-SSD', inp)
-cv.waitKey()
+        # Visualize detected bounding boxes.
+        num_detections = int(out[0][0])
+        bboxs=np.zeros([num_detections,4])
+        for i in range(num_detections):
+            classId = int(out[3][0][i])
+            score = float(out[1][0][i])
+            bbox = [float(v) for v in out[2][0][i]]
+            if classId==1:
+                bboxs[i]=bbox
+            # if score > 0.3:
+                # left = bbox[1] * cols
+                # top = bbox[0] * rows
+                # right = bbox[3] * cols
+                # bottom = bbox[2] * rows
+                # cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (0xff, 0xcc, 0x66), thickness=10)
+        if(len(bboxs)>1):
+            ratios[j]=my_utils.IoU(bboxs[0],bboxs[1])
+            count = np.sum(ratios[j-5:j] > 0.2)
+            if count >= 3:
+                visualize_fighting(img, bboxs)
+        
+        cv.imshow('TensorFlow MobileNet-SSD', inp)
+        cv.waitKey()
+varance=my_utils.var(ratios)
--- my_utils.py
+++ my_utils.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
+import numpy as np
 
 def printTensors(pb_file):
-    """https://stackoverflow.com/questions/35336648/list-of-tensor-names-in-graph-in-tensorflow/50620593#50620593
+    """ https://stackoverflow.com/questions/35336648/list-of-tensor-names-in-graph-in-tensorflow/50620593#50620593
     """
     # read pb into graph_def
     with tf.gfile.GFile(pb_file, "rb") as f:
@@ -15,3 +16,58 @@ def printTensors(pb_file):
     # print operations
     for op in graph.get_operations():
         print(op.name)
+
+        
+        
+def IoU(Reframe, GTframe):
+    """ https://www.jianshu.com/p/a4237e252087
+    自定义函数，计算两矩形 IOU，传入为均为矩形对角线，（x,y）  坐标。
+    """
+    left1 = Reframe[1]
+    top1 = Reframe[0]
+    width1 = Reframe[3] - Reframe[1]
+    height1 = Reframe[2] - Reframe[0]
+
+    left2 = GTframe[1]
+    top2 = GTframe[0]
+    width2 = GTframe[3] - GTframe[1]
+    height2 = GTframe[2] - GTframe[0]
+
+    # endx = max(x1 + width1, x2 + width2)
+    # startx = min(x1, x2)
+    # width = width1 + width2 - (endx - startx)
+    #
+    # endy = max(y1 + height1, y2 + height2)
+    # starty = min(y1, y2)
+    # height = height1 + height2 - (endy - starty)
+
+    #另一种求重叠区域面积的方式
+    endx = min(left1+width1,left2+width2)
+    startx = max(left1,left2)
+    width = endx -startx
+
+    endy = min(top1+height1,top2+height2)
+    starty = max(top1,top2)
+    height = endy - starty
+
+    if width <= 0 or height <= 0:
+        ratio = 0
+    else:
+        Area = width * height
+        Area1 = width1 * height1
+        Area2 = width2 * height2
+        ratio = Area * 1. / (Area1 + Area2 - Area)
+
+    return ratio
+
+
+
+def var(nlist):
+    N=len(nlist)
+    narray = np.array(nlist)
+    sum1 = narray.sum()
+    narray2 = narray * narray
+    sum2 = narray2.sum()
+    mean = sum1 / N
+    ans = sum2 / N - mean ** 2
+    return ans
